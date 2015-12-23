041efda5b0708c5161927eea93f6e7c71668de20
samk64 <samk64@github.com>
2015-12-22 21:30:13 -0600

    necklace - read files

--- 1.1/necklace/necklace.cpp
+++ 1.1/necklace/necklace.cpp
@@ -4,9 +4,24 @@ LANG: C++
 PROB: beads
  **/
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
 
 int main () 
 {
-    std::cout << "hello" << std::endl;
+    std::ofstream ofs;
+    std::ifstream ifs;
+    ifs.open("bead.in");
+    ofs.open("bead.out");
+
+    std::string numBeads;
+    std::string chars;
+    
+    ifs >> numBeads;
+    int N = atoi(numBeads.c_str()); 
+    ifs >> chars;
+    std::cout << numBeads << std::endl 
+              << chars    << std::endl; 
+    
     return 0;
 }
